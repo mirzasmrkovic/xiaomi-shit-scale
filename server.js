7fd1dd51c64eab4e1b9ec65895b35b5209e8583b
Mirza Smrkovic <mirza.smrkovic@gmail.com>
2020-06-11 20:57:19 +0200

    server uses port 3001 locally

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import socket from 'socket.io'
 import { connect } from './utils/db.js'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const dbPort =
   process.env.DB_PORT || 'mongodb://localhost:27017/xiaomi-scoreboard'
 
